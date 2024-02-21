4b758020803e336f1d751cfe864ac7fa07f0fef9
angelShlesser <angelShlesser2010@gmail.com>
2024-02-21 22:32:27 +0100

    Lesson 20

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
 
+import Lesson20 from './lessons/Lesson20/Lesson20';
+
 function App() {
-  return (<div className="App">This in React TS App</div>);
+  return (<div className="App">
+    <Lesson20 />
+  </div>);
 }
 
 export default App;
---
+++ src/lessons/Lesson20/Lesson20.tsx
@@ -0,0 +1,32 @@
+function Lesson20() {
+  // 1. тип string: Типизация строк - строки все что идет в дв-х, одинарных, литеральных кавычках
+  let someString: string = "This is string";
+  console.log(someString);
+  someString = "This is changed string";
+  console.log(someString);
+  // someString = 5; // так делать нельзя - будет ошибка т.к 5 - это число, а someString - имеет тип данных string
+  // let someString2: string = 10; - // так тоже делать нельзя - будет ошибка
+  
+  // 2 тип number
+  let someNumber: number = 30; // number может быть с плавующей точкой
+  // someNumber = 30 + "5" будет ошибка, т.к "5" - это строка
+  let numberWithDot: number = 100.375;
+
+  // 3 тип - boolean - только false или true
+  let isActive: boolean = true;
+  isActive = false;
+
+  // 4 тип массивы - string[]
+  const animals: string[] = ["Cat", "Dog", "Lion"]
+  const numbers: number[] = [2, 3, 1]
+  animals.push("Fly")
+  // animals.push(1000) в String только строчки.
+  const number2 = [1,2,3,4,5]
+
+  // 4.1 тип tupel - [string, number] -позволяет создать массив фиксированной длины, с определенным типом данных. Важен порядок.
+  let arrayWithDifferentTypes:[string,string,number, boolean] = ["name","John", 3, false]
+
+    return <div>Lesson 20</div>;
+  }
+  
+  export default Lesson20;
