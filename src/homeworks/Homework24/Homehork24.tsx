141bc13749854bf0dfc3121030dc6d6fd433dd31
Vitali <angelShlesser2010@gmail.com>
2024-03-03 22:15:26 +0200

    fix comment

--- src/homeworks/Homework24/Homehork24.tsx
+++ src/homeworks/Homework24/Homehork24.tsx
@@ -1,39 +1,48 @@
 import { useState, useEffect } from "react";
-import {Container, Homework24Wrapper, ErrorParagraph, Title, JokeText, GlobalStyles} from "./styles"
+import { Container, Homework24Wrapper, ErrorParagraph, Title, JokeText, GlobalStyles } from "./styles";
 import Button from "components/Button/Button";
 import { Global } from "@emotion/react";
-import {Joke} from "./type"
+import { Joke } from "./type";
 
 function Homework24() {
+  // State для хранения полученной шутки и ошибки
   const [joke, setJoke] = useState<Joke | null>(null);
   const [error, setError] = useState<string | null>(null);
 
+  // Функция для отправки запроса на получение шутки
   const getJoke = async () => {
     try {
       const response = await fetch("https://official-joke-api.appspot.com/random_joke");
       const result = await response.json();
 
       if (!response.ok) {
+        // Если ответ сервера не успешен, генерируем ошибку
         throw new Error("API Error");
       } else {
+        // Если успешно, обновляем state с шуткой и сбрасываем ошибку
         setJoke(result);
         setError(null);
       }
     } catch (error) {
+      // В случае ошибки при запросе, обновляем state с ошибкой и сбрасываем шутку
       setJoke(null);
       setError("Ошибка при получении данных");
     }
   };
 
+  // Эффект, вызываемый при первичной загрузке компонента
   useEffect(() => {
     getJoke();
   }, []);
 
+  // Обработчик клика по кнопке для получения новой шутки
   const handleButtonClick = () => {
     getJoke();
   };
 
+  // Эффект, реагирующий на изменения в state с шуткой или ошибкой
   useEffect(() => {
+    // Выводит алерт в случае получения новой шутки или ошибки
     if (joke) {
       alert("Вы получили новую шутку");
     } else if (error) {
@@ -46,7 +55,7 @@ function Homework24() {
       {/* Применяем глобальные стили к корневому элементу компонента */}
       <Global styles={GlobalStyles} />
 
-      {/* Остальные компоненты с использованием стилей */}
+      {/* Рендер компонентов с использованием стилей */}
       <Homework24Wrapper>
         <Title>Random Joke</Title>
         {error && <ErrorParagraph>{error}</ErrorParagraph>}
@@ -56,10 +65,11 @@ function Homework24() {
             <JokeText>{joke.punchline}</JokeText>
           </Container>
         )}
+        {/* Кнопка для запроса новой шутки */}
         <Button name="Get a new joke" onClick={handleButtonClick} />
       </Homework24Wrapper>
     </>
   );
-};
+}
 
 export default Homework24;
